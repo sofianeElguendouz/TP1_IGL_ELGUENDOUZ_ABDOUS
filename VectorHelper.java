14ea252d8ad63254962ecb22de815d21987fb431
abdouskamel <32772484+abdouskamel@users.noreply.github.com>
2017-10-13 16:54:46 +0200

    Ajout de sommerVecteurs
    Ajout de la méthode sommerVecteurs

--- VectorHelper.java
+++ VectorHelper.java
@@ -1,3 +1,17 @@
+import java.util.ArrayList;
+
 public class VectorHelper 
 {
+  public static ArrayList<Integer> sommerVecteurs(ArrayList<Integer> vect1, ArrayList<Integer> vect2)
+  {
+    if(vect1.size() != vect2.size())
+      throw new IllegalArgumentException("La taille des deux vecteurs est différentes.");
+    
+    ArrayList<Integer> retVect = new ArrayList<>(vect1.size());
+    
+    for(int i = 0; i < vect1.size(); ++i)
+      retVect.add(vect1.get(i) + vect2.get(i));
+    
+    return retVect;
+  }
 }
