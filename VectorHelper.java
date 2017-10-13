cf182aea0a88d90add8092a7b42df6247bb8ed07
abdouskamel <32772484+abdouskamel@users.noreply.github.com>
2017-10-13 17:07:28 +0200

    Documentation
    Ajout de la documentation sur la classe et sur la fonction sommerVecteurs.

--- VectorHelper.java
+++ VectorHelper.java
@@ -1,7 +1,18 @@
 import java.util.ArrayList;
 
+/**
+ * Classe définissant quelques méthodes de manipulation de vecteurs.
+ * @version 1.0
+ */
 public class VectorHelper 
 {
+  /**
+   * Calcule la somme de deux vecteurs. Les deux vecteurs doivent avoir la même taille.
+   * @param vect1 Premier vecteur.
+   * @param vect2 Deuxième vecteur.
+   * @return Le vecteur dont le contenu de la case i est vect1[i] + vect2[i].
+   * @exception IllegalArgumentException dans le cas où les vecteurs n'ont pas la même taille.
+   */
   public static ArrayList<Integer> sommerVecteurs(ArrayList<Integer> vect1, ArrayList<Integer> vect2)
   {
     if(vect1.size() != vect2.size())
